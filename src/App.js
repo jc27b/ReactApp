a633e9361ecbb7dbf26b2a39dc6585084dabc755
Juan Camilo <juancamilo96b@hotmail.com>
2025-03-06 02:01:04 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,18 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
